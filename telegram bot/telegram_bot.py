961c4f52c5712f6b453175938ce6ae4ba354f3e1
Apuri-Amarnath <apuriamarnath123@gmail.com>
2023-05-10 23:25:30 +0530

    added changes

--- telegram bot/telegram_bot.py
+++ telegram bot/telegram_bot.py
@@ -1,5 +1,6 @@
 import telegram.ext
 import os
+
 Token = "5762000645:AAGcXqZTVPl2hxvJJ2WpbiEfneDlRsllZhQ"
 
 updater = telegram.ext.Updater(
@@ -16,9 +17,13 @@ def help(update, context):
     update.message.reply_text('''
     The following commands are avialable:
 
-    /start --> Weclome to the file shareing
+    /start --> Weclome to the notes sharing
     /help --> this particular message
     /E5 --> electronics 5th semster
+    /E6 --> electronics 6th semster
+    /M5 --> Mathematics 5th semster
+    /M6 --> mathematics 6th semster
+    
     /stop --> stop bot'''
                               )
 
@@ -28,15 +33,29 @@ def content(update, context):
         "we have various courses notes and question papers")
 
 
-def electronics5(update, context):
+def E5(update, context):
     update.message.reply_text("Here is the notes of E5")
+
+def E6(update, context):
+    update.message.reply_text("Here is the notes of E6")
+
+
+def M5(update, context):
+    update.message.reply_text("Here is the notes of M5")
+
+def M6(update, context):
+    update.message.reply_text("Here is the notes of M6")
 
 
 dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
 
 dispatcher.add_handler(telegram.ext.CommandHandler('help', help))
 
-dispatcher.add_handler(telegram.ext.CommandHandler('E5', electronics5))
+dispatcher.add_handler(telegram.ext.CommandHandler('E5', E5))
+dispatcher.add_handler(telegram.ext.CommandHandler('E6', E6))
+
+dispatcher.add_handler(telegram.ext.CommandHandler('M5', M5))
+dispatcher.add_handler(telegram.ext.CommandHandler('M6', M6))
 
 updater.start_polling()
 updater.idle()
