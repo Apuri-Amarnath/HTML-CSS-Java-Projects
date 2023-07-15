6d6b1b86e429c2a2b8014021ce7860b1d4973613
Apuri-Amarnath <110279434+Apuri-Amarnath@users.noreply.github.com>
2023-07-15 17:57:25 +0530

    Add files via upload

---
+++ projects/conference page/script.js
@@ -0,0 +1,13 @@
+
+function change() {
+    var element = document.getElementById("container");
+    element.style.display = "none";
+    var element1 = document.getElementById('otherdiv');
+    element1.style.display = "block";
+}
+function Back(){
+    var element = document.getElementById("container");
+    element.style.display = "block";
+    var element1 = document.getElementById("otherdiv");
+    element1.style.display = "none";
+}
