d55a5a60e16400b27c1d8305ed2e06d60649aefd
Apuri-Amarnath <110279434+Apuri-Amarnath@users.noreply.github.com>
2023-07-24 12:42:00 +0530

    completed

---
+++ projects/music page/script.js
@@ -0,0 +1,13 @@
+
+function back() {
+    Element1 = document.getElementById("page-1");
+    Element2 = document.getElementById("page-2")
+    Element1.style.display = "block";
+    Element2.style.display = "none";
+}
+function change() {
+    Element1 = document.getElementById("page-1");
+    Element2 = document.getElementById("page-2")
+    Element2.style.display = "block";
+    Element1.style.display = "none";
+}
